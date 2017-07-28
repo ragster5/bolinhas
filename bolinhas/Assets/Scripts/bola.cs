55e374e3b7b7e112d1dc5132bde581e0028bb8c8
Ragster <alex_xandre93@hotmail.com>
2017-07-28 16:05:15 -0300

    initial project version

---
+++ bolinhas/Assets/Scripts/bola.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bola : MonoBehaviour {
+
+	private SpriteRenderer figura;
+	private int	codigoCor;
+
+
+	// Use this for initialization
+	void Start () {
+		figura = GetComponent<SpriteRenderer> ();
+		figura.color = sorteioCor ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void setCodigoCor(int codigo){
+		this.codigoCor = codigo;
+
+	}
+	private Color sorteioCor(){
+		int sorteio = Random.Range (1, 11);
+		switch (sorteio) {
+		case 1:
+			setCodigoCor (1);
+			return Color.black;
+		case 2:
+			setCodigoCor (2);
+			return Color.blue;
+		case 3:
+			setCodigoCor (3);
+			return Color.cyan;
+		case 4:
+			setCodigoCor (4);
+			return Color.gray;
+		case 5:
+			setCodigoCor (5);
+			return Color.green;
+		case 6:
+			setCodigoCor (6);
+			return Color.grey;
+		case 7:
+			setCodigoCor (7);
+			return Color.magenta;
+		case 8:
+			setCodigoCor (8);
+			return Color.red;
+		case 9:
+			setCodigoCor (9);
+			return Color.white;
+		case 10:
+			setCodigoCor (10);
+			return Color.yellow;
+		}
+		return Color.black;
+	}
+}
---
+++ bolinhas/Assets/Scripts/spawnBolas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnBolas : MonoBehaviour {
+
+	public Transform	limiteCima, limiteBaixo, limiteEsquerda, limiteDireita;
+	public GameObject	bola;
+	private float		timer;
+	public float		tempoSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		
+	}
+}
